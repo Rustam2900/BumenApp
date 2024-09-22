8b0665d3d60b5b504440526faad007213ac53d03
Behruz <xoliqbediyevebuz@gmail.com>
2024-09-22 21:10:08 +0500

    feat(account): add reset password views

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
 from rest_framework import generics, views, status, permissions
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
 from account import models, serializers
 
@@ -44,3 +45,65 @@ class RegisterVerifyApiView(generics.GenericAPIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class ResetPasswordRequestApiView(generics.GenericAPIView):
+    serializer_class = serializers.ResetPasswordRequestSerializer
+
+    def post(self, request):
+        serializer = serializers.ResetPasswordRequestSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            phone_number = serializer.data['phone_number']
+            try:
+                user = models.User.objects.get(phone_number=phone_number)
+                code = user.generate_verify_code()
+                return Response({"message": serializer.data, "code": code}, status=status.HTTP_200_OK)
+            except models.User.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPasswordVerifyApiView(generics.GenericAPIView):
+    serializer_class = serializers.ResetPasswordVerifySerializer
+
+    def post(self, request):
+        serializer = serializers.ResetPasswordVerifySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            code = serializer.data['code']
+            try:
+                otp_code = models.UserOtpCode.objects.get(code=code)
+                otp_code.is_used = True
+                user = otp_code.user
+                token = RefreshToken.for_user(user).access_token
+                if user:
+                    return Response({"message": 'code tasdiqlandi', 'token': str(token)}, status=status.HTTP_200_OK)
+                else:
+                    return Response({"message": 'code tasdiqlanmadi'}, status=status.HTTP_400_BAD_REQUEST)
+            except models.UserOtpCode.DoesNotExist:
+                return Response({'message': 'code is invalid'},status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResetPasswordApiView(generics.GenericAPIView):
+    serializer_class = serializers.ResetPasswordSerializer
+
+    def post(self, request, *args, **kwargs):
+        token = kwargs.get('token')
+        if not token:
+            return Response({'message': 'token topilmadi'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            access_token = AccessToken(token)
+            user = models.User.objects.get(id=access_token['user_id'])
+        except Exception as e:
+            return Response({"detail": "Token noto'g'ri yoki muddati o'tgan"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = serializers.ResetPasswordSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            new_password = serializer.data['new_password']
+            user.set_password(new_password)
+            user.save()
+
+            return Response({'message': 'Parol muvaffaqiyatli ozgartirildi'}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
